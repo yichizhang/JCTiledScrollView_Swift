753991fe852948c65245b51130b40995edb9cde9
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-01-26 02:25:45 +1100

    Changed copyright information

--- JCTiledScrollView_Swift_Source/JCAnnotation.swift
+++ JCTiledScrollView_Swift_Source/JCAnnotation.swift
@@ -1,11 +1,27 @@
-//
-//  JCAnnotation.swift
-//  JCTiledScrollView-Swift
-//
-//  Created by Yichi on 7/01/2015.
-//  Copyright (c) 2015 Yichi Zhang. All rights reserved.
-//
+/*
 
+ JCTiledScrollView_Swift
+
+ Based on the code by:
+
+ Jesse Collis (jessedc)
+ Julius Oklamcak (vfr)
+ Pete Hare (petehare)
+ Anthony Damotte (adamotte)
+
+
+
+ Copyright (c) 2015 Yichi Zhang
+ https://github.com/yichizhang
+ zhang-yi-chi@hotmail.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
 import UIKit
 
 @objc protocol JCAnnotation : NSObjectProtocol {
