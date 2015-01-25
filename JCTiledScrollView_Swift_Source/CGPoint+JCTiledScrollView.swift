0b6a36cacfd14cba4e9d3529e17db8a24577cea3
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-01-25 22:36:42 +1100

    Changed "point:isWithinBounds:" to a CGPoint extension method

---
+++ JCTiledScrollView_Swift_Source/CGPoint+JCTiledScrollView.swift
@@ -0,0 +1,16 @@
+//
+//  CGPoint+JCTiledScrollView.swift
+//  JCTiledScrollView-Swift
+//
+//  Created by Yichi on 25/01/2015.
+//  Copyright (c) 2015 Yichi Zhang. All rights reserved.
+
+import Foundation
+
+extension CGPoint{
+	
+	func jc_isWithinBounds(bounds:CGRect) -> Bool{
+		return CGRectContainsPoint(CGRectInset(bounds, -25.0, -25.0), self)
+	}
+	
+}
