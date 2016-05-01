dd82d0aa1df46b08ffe7b39cc6003a18034444e1
Yichi Zhang <zhang-yi-chi@hotmail.com>
2016-05-01 15:24:04 +1000

    Fix init method and reuseIdentifier in JCAnnotationView

--- Demo/Demo-Swift/Demo-Swift/DemoAnnotationView.swift
+++ Demo/Demo-Swift/Demo-Swift/DemoAnnotationView.swift
@@ -12,17 +12,18 @@ import UIKit
 	
 	var imageView:UIImageView!
 	
-	convenience init(frame: CGRect, annotation:JCAnnotation, reuseIdentifier:NSString) {
-		
-		self.init(frame: frame)
-		
-		self.annotation = annotation
-		self.reuseIdentifier = reuseIdentifier
-	
+	override init(frame: CGRect, annotation: JCAnnotation, reuseIdentifier: String)
+    {
+        super.init(frame: frame, annotation: annotation, reuseIdentifier: reuseIdentifier)
+        
 		imageView = UIImageView()
 		addSubview(imageView)
-		
 	}
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
 	
 	override func sizeThatFits(size: CGSize) -> CGSize {
 		return CGSizeMake(max(imageView.image!.size.width,30), max(imageView.image!.size.height,30))
